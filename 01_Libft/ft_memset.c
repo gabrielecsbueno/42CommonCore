1c64cc53808ea3152aa8b394bbb4609500d23665
Gabriele Bueno <gabrielecsbueno@gmail.com>
2025-10-07 13:13:30 +0200

    ft_memset.c

---
+++ 01_Libft/ft_memset.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabde-so <gabde-so@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/04 08:48:31 by gabde-so          #+#    #+#             */
+/*   Updated: 2025/10/06 11:37:00 by gabde-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	int		i;
+	size_t	size;
+
+	size = sizeof(s[i]); // ver se é 4 ou 1
+	if(size == 1)
+		//copio o bites
+
+	i = c;
+	while(i > 0)
+	{
+		c =
+		i--;
+	}
+}
