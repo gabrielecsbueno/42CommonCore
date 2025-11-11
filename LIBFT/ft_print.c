5d73ea64ac85adb5ce2b5a38775926a47fb927b5
Gabriele Bueno <gabrielecsbueno@gmail.com>
2025-11-11 16:50:54 +0100

    LIBFT ATUALIZADA (add printf)

---
+++ LIBFT/ft_print.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabde-so <gabde-so@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/31 09:46:57 by gabde-so          #+#    #+#             */
+/*   Updated: 2025/11/11 16:38:33 by gabde-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_putchar(char c)
+{
+	return (write (1, &c, 1));
+}
+
+int	ft_putstr(const char	*str)
+{
+	int		i;
+
+	i = 0;
+	if (!str)
+		return (ft_putstr("(null)"));
+	while (str[i])
+	{
+		write (1, &str[i], 1);
+		i++;
+	}
+	return (i);
+}
+
+int	ft_putnbr(int n)
+{
+	int		size;
+
+	size = 0;
+	if (n == -2147483648)
+		return (ft_putstr("-2147483648"));
+	if (n >= 0 && n <= 9)
+		size += ft_putchar(n + '0');
+	else if (n > 9)
+	{
+		size += ft_putnbr(n / 10);
+		size += ft_putnbr(n % 10);
+	}
+	else if (n < 0)
+	{
+		size += ft_putchar('-');
+		size += ft_putnbr(n * -1);
+	}
+	return (size);
+}
+
+int	ft_putunsigned(unsigned int n)
+{
+	int	size;
+
+	size = 0;
+	if (n <= 9)
+		size += ft_putchar(n + '0');
+	else if (n > 9)
+	{
+		size += ft_putunsigned(n / 10);
+		size += ft_putunsigned(n % 10);
+	}
+	return (size);
+}
+
+int	ft_puthexa(unsigned long n, char type)
+{
+	int	size;
+
+	size = 0;
+	if (type == 'p')
+	{
+		if (!n)
+			return (ft_putstr("(nil)"));
+		size += ft_putstr("0x");
+		type = 'x';
+	}
+	if (n >= 16)
+		size += ft_puthexa (n / 16, type) + ft_puthexa (n % 16, type);
+	else
+	{
+		if (n >= 10)
+		{
+			if (type == 'X')
+				size += ft_putchar(n + 55);
+			else
+				size += ft_putchar(n + 87);
+		}
+		else
+			size += ft_putchar('0' + n);
+	}
+	return (size);
+}
---
+++ LIBFT/ft_printf.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabde-so <gabde-so@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/29 22:23:47 by gabde-so          #+#    #+#             */
+/*   Updated: 2025/11/11 16:38:18 by gabde-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_type(char format);
+static int	ft_printtype(char print, va_list argument);
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	arguments;
+	size_t	i;
+	int		size;
+
+	if (!format)
+		return (-1);
+	va_start (arguments, format);
+	i = 0;
+	size = 0;
+	while (format[i])
+	{
+		if (format[i] != '%')
+			size += ft_putchar(format[i]);
+		else if (format[i] == '%' && ft_type(format[i + 1]) == 0)
+		{
+			va_end(arguments);
+			return (-1);
+		}
+		else
+			size += ft_printtype(format[++i], arguments);
+		i++;
+	}
+	va_end(arguments);
+	return (size);
+}
+
+static int	ft_type(char format)
+{
+	if (format == 'c' || format == 's' || format == 'p' || format == 'd'
+		|| format == 'i' || format == 'u' || format == 'x' || format == 'X'
+		|| format == '%')
+	{
+		return (1);
+	}
+	return (0);
+}
+
+static int	ft_printtype(char print, va_list argument)
+{
+	int	n;
+
+	n = 0;
+	if (print == 'c')
+		n = ft_putchar((char)va_arg(argument, int));
+	else if (print == 's')
+		n = ft_putstr(va_arg(argument, char *));
+	else if (print == 'd' || print == 'i')
+		n = ft_putnbr(va_arg(argument, int));
+	else if (print == 'u')
+		n = ft_putunsigned(va_arg(argument, unsigned int));
+	else if (print == 'p')
+		n = ft_puthexa((unsigned long)va_arg(argument, void *), print);
+	else if (print == 'x' || print == 'X')
+		n = ft_puthexa((unsigned int)va_arg(argument, int), print);
+	else if (print == '%')
+		n = ft_putchar('%');
+	return (n);
+}
