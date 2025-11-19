a0f91922923f3174fe26d5a2292bbc7ff5dcc4a8
Gabriele Bueno <gabrielecsbueno@gmail.com>
2025-11-19 22:29:27 +0100

    GNL (v08.1)

--- M1/03_GNL/get_next_line.c
+++ M1/03_GNL/get_next_line.c
@@ -6,12 +6,87 @@
 /*   By: gabde-so <gabde-so@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 15:26:16 by gabde-so          #+#    #+#             */
-/*   Updated: 2025/11/18 10:20:16 by gabde-so         ###   ########.fr       */
+/*   Updated: 2025/11/19 18:08:08 by gabde-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*get_next_line(int fd)
+{
+	static char	*resto = NULL;
+	size_t		bytes_read;
+	char		*buffer;
+	int			index;
+	char		*temp;
+	char		*line;
+
+	// verificacoes iniciais
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+
+	// aloca buffer
+	buffer = (char *)malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+
+	// inicializa resto corretamente
+	if (!resto)
+		resto = ft_strdup("");
+
+	// loop principal - busca por \n
+	while (ft_strindex(resto, '\n') == -1)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+
+		// erro na leitura
+		if (bytes_read < 0)
+		{
+			free(buffer);
+			free(resto);  // free(NULL) e mais seguro
+			resto = NULL;
+			return (NULL);
+		}
+
+		// fim do arquivo
+		if (bytes_read == 0)
+		{
+			free(buffer);
+			// se resto tem conteúdo, retorna
+			if (resto && resto[0] != '\0')
+			{
+				line = ft_strdup(resto);
+				free(resto);
+				resto = NULL;
+				return (line);
+			}
+			// se resto esta vazio, retorna NULL
+			if (resto)  // PEQUENA CORREÇÃO AQUI
+				free(resto);
+			resto = NULL;
+			return (NULL);
+		}
+
+		// termina a string corretamente com \0
+		buffer[bytes_read] = '\0';
+
+		// junta ao resto
+		temp = resto;
+		resto = ft_strjoin(temp, buffer);
+		free(temp);
+	}
+
+	// encontrou \n - extrai linha
+	free(buffer);
+	index = ft_strindex(resto, '\n');
+	temp = resto;
+	line = ft_substr(resto, 0, index + 1); // INCLUI o \n!!!!!!!
+	resto = ft_substr(temp, index + 1, ft_strlen(temp) - index - 1);
+	free(temp);
+	return (line);
+}
+
+/*
 char	*get_next_line(int fd)
 {
 	static char	*resto = NULL;
@@ -31,7 +106,7 @@ char	*get_next_line(int fd)
 		return(NULL);
 	//iniciacao do resto se nao tiver iniciado
 	if(!resto)
-		resto = (char *)malloc(1);
+		resto = ft_strdup("");
 
 //loop principal
 	//ENQUANTO NAO encontrou um \n, nao tem linha completa
@@ -55,7 +130,7 @@ char	*get_next_line(int fd)
 		}
 		//SE leu mais que 1 byte
 		//fecha ele para nao esta aberto
-		buffer[bytes_read] = '\n';
+		buffer[bytes_read] = '\0';
 		//usa um temporario para guardar o que foi pego no loop anterior
 		temp = resto;
 		//aloca o novo valor de resto com o que foi lido no loop
@@ -66,10 +141,10 @@ char	*get_next_line(int fd)
 	}
 	//ja faco free no buffer porque nao uso mais
 	free(buffer);
-//busca donde esta el \n y retorna el indice
+//busca onde esta o \n e retorna o indice
 	index = ft_strindex(resto, '\n');
 	//SE TEM \n (index)??
-	
+
 	//passa tudo para o temporario
 	temp = resto;
 	//copia o resto ate o \n para a linha
@@ -81,7 +156,7 @@ char	*get_next_line(int fd)
 	//RETORNA linha e resto atualizado
 	return (line);
 }
-
+*/
 /*
 char	*get_next_line(int fd)
 {
